29dbd1cfac0328fbfeddf05e38648dc2109c16ae
rginiunaite <rasaginiu@gmail.com>
2018-05-18 18:36:56 +0100

    Add files via upload
    works well

--- density_profiles.m
+++ density_profiles.m
@@ -22,15 +22,28 @@ M_AQup = M_AQup(:,1)/10;
  b = smooth(a)
  c = smooth(b)
  figure
- scatter (x,M_control)
-% figure 
- %plot (x, smooth(smooth(M_control)))
- %hold on
- %plot(x, smooth(smooth(M_AQup)))
+%  scatter (x,M_control)
+% % figure 
+%  %plot (x, smooth(smooth(M_control)))
+%  %hold on
+%  %plot(x, smooth(smooth(M_AQup)))
+%  
+%  x_in = linspace(min(x),max(x))
+% fu_interp = interp1(x,M_control,x_in,'spline' )
+%  hold on
+%  plot(x_in,fu_interp)
+%  plot(x, smooth(M_control))
  
- x_in = linspace(min(x),max(x))
-fu_interp = interp1(x,M_control,x_in,'spline' )
+ 
+options = fitoptions('Method','Smooth','SmoothingParam',0.00001);
+[f_control,gof,out] = fit(x',M_control,'smooth',options);
+
+ 
+plot(f_control, x',M_control)
+
+[f_AQ_up,gof,out] = fit(x',M_AQup,'smooth',options);
+
  hold on
- plot(x_in,fu_interp)
- 
+plot(f_AQ_up, x',M_AQup)
+
  
